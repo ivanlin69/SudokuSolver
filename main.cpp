6a946a4e4d0d71531184ca4ae6364f39e0584e55
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-05-17 11:09:35 +0800

    Removed test code.

--- main.cpp
+++ main.cpp
@@ -3,19 +3,16 @@
 #include "time.h"
 
 int main() {
-
-    //std::string board = "090700860031005020806000000007050006000307000500010700000000109020600350054008070";
-
+    
     SudokuSolver sudokuSolver;
     sudokuSolver.welcomeMessage();
-    //sudokuSolver.readBoard(board);
     if(sudokuSolver.readBoardFromFiles()){
+        
         clock_t a,b;
         a=clock();
-        //sudokuSolver.inferAC3();
-        //sudokuSolver.inferAC3Improved();
         sudokuSolver.inferAC3Guessing();
         b=clock();
+        
         sudokuSolver.printStatus();
         std::cout << "Puzzle solved. Time took: " << double(b)/CLOCKS_PER_SEC << "s" << std::endl;
     }
